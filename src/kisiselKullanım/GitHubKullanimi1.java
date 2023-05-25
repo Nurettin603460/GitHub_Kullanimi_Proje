1e1d0121f1104ed3158123a169b3eeba20c76799
Nurettin KAYA <nurettinkaya@gmail.com>
2023-05-25 19:45:10 +0300

    ilk degisiklik

--- src/kisiselKullanım/GitHubKullanimi1.java
+++ src/kisiselKullanım/GitHubKullanimi1.java
@@ -3,5 +3,6 @@ package kisiselKullanım;
 public class GitHubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("That's it");
     }
 }
