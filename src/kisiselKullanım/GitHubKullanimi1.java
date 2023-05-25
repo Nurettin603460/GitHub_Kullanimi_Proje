6f806188d04fe2330158ecce112fec49ab8c0d9b
Nurettin KAYA <nurettinkaya@gmail.com>
2023-05-25 22:20:31 +0300

    nurettin guncelleme

--- src/kisiselKullanım/GitHubKullanimi1.java
+++ src/kisiselKullanım/GitHubKullanimi1.java
@@ -2,11 +2,11 @@ package kisiselKullanım;
 
 public class GitHubKullanimi1 {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+
         System.out.println("That's it");
 
-
         System.out.println("nurettin branc.inde ortak class.ta degisiklik yaptim");
-
     }
 }
