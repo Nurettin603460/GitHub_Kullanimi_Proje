4ea456bb9879a664012a5729f8917ec2843e4f98
Nurettin KAYA <nurettinkaya@gmail.com>
2023-05-25 22:13:00 +0300

    main branch guncellendi

--- src/kisiselKullanım/GitHubKullanimi1.java
+++ src/kisiselKullanım/GitHubKullanimi1.java
@@ -2,7 +2,10 @@ package kisiselKullanım;
 
 public class GitHubKullanimi1 {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
         System.out.println("That's it");
+        System.out.println("main branch degisiklik 1.satir");
+        System.out.println("main branch degisiklik 2.satir");
     }
 }
