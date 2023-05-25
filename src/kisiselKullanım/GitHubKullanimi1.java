557ce6ae938f6f40772153e440624e972628293f
Nurettin KAYA <nurettinkaya@gmail.com>
2023-05-25 22:07:28 +0300

    nurettin branch.i 2.kez guncellendi

--- src/kisiselKullanım/GitHubKullanimi1.java
+++ src/kisiselKullanım/GitHubKullanimi1.java
@@ -4,5 +4,7 @@ public class GitHubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("That's it");
+        System.out.println("nurettin branc.inde ortak class.ta degisiklik yaptim");
+
     }
 }
--- src/kisiselKullanım/ozellik1.java
+++ src/kisiselKullanım/ozellik1.java
@@ -2,6 +2,8 @@ package kisiselKullanım;
 
 public class ozellik1 {
     public static void main(String[] args) {
+
         System.out.println("nurettin branch'inda calisiyorum");
+        System.out.println("nurettin branch.inda 2.degisiklik kendime ozel class.ta");
     }
 }
